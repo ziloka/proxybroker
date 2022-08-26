bdca22487715af47b804a0cbdf0dbe1b700643dd
Ziloka <50429450+ziloka@users.noreply.github.com>
2022-08-26 10:23:18 -0400

    improve performance

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -15,32 +15,38 @@ pub async fn find(sub_matches: &ArgMatches) {
   let mut counter: u64 = 0;
   let mut vecs_of_unchecked_proxies = services::collector::collect();
   'unchecked_proxies_loop: while let Some(unchecked_proxies) = vecs_of_unchecked_proxies.next().await {
-    let mut list_of_checked_proxies = services::checker::check(unchecked_proxies);
-    while let Some(proxy) = list_of_checked_proxies.next().await {
-      match proxy {
-        Ok(proxy) => {
-          if proxy.alive {
-            if let Some(ref mut value) = file {
-              match &value {
-                Ok(file) => {
-                  match file.clone().write(format!("{}:{}\n", proxy.host, proxy.port).as_bytes()) {
-                    Ok(_is_ok) => {
+    match unchecked_proxies {
+      Ok(unchecked_proxies) => {
+        let mut list_of_checked_proxies = services::checker::check(unchecked_proxies);
+        while let Some(proxy) = list_of_checked_proxies.next().await {
+          match proxy {
+            Ok(proxy) => {
+              if proxy.alive {
+                if let Some(ref mut value) = file {
+                  match &value {
+                    Ok(file) => {
+                      match file.clone().write(format!("{}:{}\n", proxy.host, proxy.port).as_bytes()) {
+                        Ok(_is_ok) => {
+                        },
+                        Err(err) => println!("Could not write to file: {}", err)
+                      }
                     },
                     Err(err) => println!("Could not write to file: {}", err)
                   }
-                },
-                Err(err) => println!("Could not write to file: {}", err)
+                }
+                counter+=1;
+                println!("{}:{}", proxy.host, proxy.port);
               }
-            }
-            counter+=1;
-            println!("{}:{}", proxy.host, proxy.port);
+            },
+            Err(_err) => {}
           }
-        },
-        Err(_err) => {}
+          if counter >= *limit {
+            break 'unchecked_proxies_loop;
+          }
+        }
       }
-      if counter >= *limit {
-        break 'unchecked_proxies_loop;
-      }
+      Err(err) => println!("Could not join task: {}", err)
     }
+
   }
 }
--- src/services/collector.rs
+++ src/services/collector.rs
@@ -1,54 +1,60 @@
-use std::future::Future;
-use futures::stream::FuturesUnordered;
-use serde::Deserialize;
+use futures::{stream::futures_unordered::FuturesUnordered, Future, FutureExt};
 use regex::Regex;
+use serde::Deserialize;
+use std::pin::Pin;
 
 // https://serde.rs/field-attrs.html
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Source {
-  #[serde(rename(deserialize = "url"))]
-  url: String,
-  #[serde(rename(deserialize = "type"))]
-  protocol: String,
+    #[serde(rename(deserialize = "url"))]
+    url: String,
+    #[serde(rename(deserialize = "type"))]
+    protocol: String,
 }
 
 pub struct Proxy {
-  pub host: String,
-  pub port: u16
+    pub host: String,
+    pub port: u16,
 }
 
-fn get_proxy_sources () -> Vec<Source> {
-  // https://stackoverflow.com/questions/27140634/how-to-embed-resources-in-rust-executable
-  // https://stackoverflow.com/questions/25505275/is-there-a-good-way-to-include-external-resource-data-into-rust-source-code
-  serde_json::from_str(&String::from_utf8_lossy(include_bytes!("../assets/sources.json"))).unwrap()
+fn get_proxy_sources() -> Vec<Source> {
+    // https://stackoverflow.com/questions/27140634/how-to-embed-resources-in-rust-executable
+    // https://stackoverflow.com/questions/25505275/is-there-a-good-way-to-include-external-resource-data-into-rust-source-code
+    serde_json::from_str(&String::from_utf8_lossy(include_bytes!(
+        "../assets/sources.json"
+    )))
+    .unwrap()
 }
 
 async fn get_proxies_from_site(url: String) -> Vec<Proxy> {
-  let re = Regex::new(r"(\d+\.\d+\.\d+\.\d+):(\d+)").unwrap();
-  let mut proxies: Vec<Proxy> = Vec::new();
-  let request = reqwest::get(&url);
+    let re = Regex::new(r"(\d+\.\d+\.\d+\.\d+):(\d+)").unwrap();
+    let mut proxies: Vec<Proxy> = Vec::new();
+    let request = reqwest::get(&url);
     match request.await {
-      Ok(response) => {
-        match response.text().await {
-          Ok(body) => {
-            for caps in re.captures_iter(&body) {
-              proxies.push(Proxy { host: caps.get(1).unwrap().as_str().to_string(), port: caps.get(2).unwrap().as_str().parse::<u16>().unwrap() })
-            }
-          },
-          Err(e) => println!("Problem while getting request body: {}", e)
-        };
-      },
-      Err(e) => println!("Problem while executing get request: {}", e)
+        Ok(response) => {
+            match response.text().await {
+                Ok(body) => {
+                    for caps in re.captures_iter(&body) {
+                        proxies.push(Proxy {
+                            host: caps.get(1).unwrap().as_str().to_string(),
+                            port: caps.get(2).unwrap().as_str().parse::<u16>().unwrap(),
+                        })
+                    }
+                }
+                Err(e) => println!("Problem while getting request body: {}", e),
+            };
+        }
+        Err(e) => println!("Problem while executing get request: {}", e),
     };
     proxies
 }
 
 // https://www.reddit.com/r/rust/comments/dh99xn/help_multiple_http_requests_on_a_singlethread/
-pub fn collect() -> FuturesUnordered<impl Future<Output = Vec<Proxy>>> {
+pub fn collect() -> FuturesUnordered<tokio::task::JoinHandle<Vec<Proxy>>> {
   let list_of_futures = FuturesUnordered::new();
   for proxy_source in get_proxy_sources() {
-    list_of_futures.push(get_proxies_from_site(proxy_source.url));
+    list_of_futures.push(tokio::spawn(get_proxies_from_site(proxy_source.url)));
   }
   list_of_futures
 }
