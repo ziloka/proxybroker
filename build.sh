6bfd7595d584fdca51187dcc4f2736a4261e6963
Ziloka <50429450+ziloka@users.noreply.github.com>
2022-11-01 16:31:16 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-cargo build --release
 
-upx --best --lzma target/release/proxybroker
-ls -lhsa target/release/proxybroker
+# https://github.com/johnthagen/min-sized-rust#optimize-libstd-with-build-std
+
+host=$(rustc -Vv | grep -oP "(?<=host: )[\w_-]+")
+
+# Setup dependencies
+rustup toolchain install nightly
+rustup component add rust-src --toolchain nightly
+
+# Compile
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target $host --release
+
+# Run post build tasks
+upx --best --lzma target/$host/release/proxybroker
+ls -lhsa target/$host/release/proxybroker
